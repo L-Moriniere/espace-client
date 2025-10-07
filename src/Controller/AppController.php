0ff90eee89798b880ee7d17ade66dc7735b555fd
Logan Moriniere <71821650+L-Moriniere@users.noreply.github.com>
2025-10-07 15:18:20 +0200

    création entités user + message, fonction register, security, test register

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 #[Route('api', name: 'api_')]
 final class AppController extends AbstractController
 {
@@ -17,12 +24,24 @@ final class AppController extends AbstractController
         ]);
     }
 
-    #[Route('/register', name: 'app_register')]
-    public function register(): Response
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher, ValidatorInterface $validator): JsonResponse
     {
-        return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
-        ]);
+        $data = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            return $this->json($errors, 400);
+        }
+        $user->setPassword($hasher->hashPassword($user, $data['password']));
+        $user->setRoles(['ROLE_USER']);
+        $em->persist($user);
+        $em->flush();
+        return $this->json(['message' => 'Utilisateur créé avec succès'], 201);
     }
 
 
